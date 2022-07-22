8dce575ba1d6af1044ac589768509bf5e52f4d1c
Jorge Ramos <JorgeR@sandata.com>
2022-07-22 15:06:37 -0400

    finished 19 problems of the sql practice problems book

--- SQLPracticeProblems.sql
+++ SQLPracticeProblems.sql
@@ -37,4 +37,69 @@ select ProductID, ProductName from Products
 where ProductName like '%queso%'
 
 
---8 
+--8  return OrderId, CustomerId, and Shipcountry for the orders where shipcountry is either france or Belgium
+select OrderID, CustomerID, ShipCountry from Orders
+where ShipCountry in ('france', 'Belgium')
+
+--or
+select OrderID, CustomerID, ShipCountry from Orders
+where ShipCountry ='france' 
+or ShipCountry='Belgium'
+
+--9return all orders fromany latin american country. brazil , mexico , argentina venenzuela
+
+select OrderID, CustomerID, ShipCountry from Orders
+where ShipCountry in ('Brazil','Mexico', 'Argentina','Venezuela')
+
+--10 return firstname, lastname, title birthdate order results by birthdate oldest employee first
+select FirstName,LastName, Title,BirthDate from Employees 
+order by BirthDate 
+
+--11  return the above query but show only the date portion  of the birthdate field
+select FirstName,LastName, Title, cast(birthdate as date)  from Employees 
+order by BirthDate 
+--or
+select FirstName,LastName, Title, convert(date,birthdate)  from Employees 
+order by BirthDate 
+
+--12 return firstname and lastname columns from the employees table then create a new columns fullname 
+select FirstName,LastName, (firstname+' '+Lastname) as FullName from Employees
+--or 
+select FirstName,LastName, CONCAT(FirstName,' ', LastName) as FullName from Employees
+
+--13in orderdetaisl show orderid,productid,unitprice and quantity , create a new field totalprice (unitprice * quantity) . order by orderid and product id 
+select OrderID,ProductID,UnitPrice,Quantity,(unitprice *quantity) as totalPrice from [Order Details]
+order by OrderID, ProductID
+
+
+--14 show the total number of customers that we have in customers table. show only one value. 
+
+select count(CustomerID) from Customers
+
+
+--15 show the date of the first order ever made in the orders table
+
+select  Min( OrderDate) from Orders
+--or 
+select top 1 OrderDate from orders
+
+--16 list of countries where the northwind company has customers
+select distinct Country from Customers
+--or
+select Country from Customers
+group by Country
+
+--17 list of all the different values in the customers table for contacttitles. also include a count for each contact title. 
+select ContactTitle,count(CustomerID)as totalContactTitle  from customers
+group by ContactTitle
+order by totalContactTitle desc
+
+--18 show for each product the asssociated supplier . show product id, product name  and companyname of the supplier. sort by product id
+--using both tables
+select ProductID,ProductName,s.CompanyName from Suppliers s
+join products p
+on s.SupplierID = p.SupplierID
+order by productId
+
+
+--19
